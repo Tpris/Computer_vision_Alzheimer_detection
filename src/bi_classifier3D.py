abcbc0268ea1681bfc38173bc2306ad5444afbb2
Tpris <priscill@live.fr>
2023-12-09 16:44:31 +0100

    accuracy a 75/80%

--- src/bi_classifier3D.py
+++ src/bi_classifier3D.py
@@ -13,31 +13,32 @@ class Biclassifier3D():
         self.kernel_size = kernel_size
         self.activation = activation
         self.dropout = dropout
+        self.initializer = tf.keras.initializers.GlorotNormal()
+
     
     def build_model(self):
         input = Input(shape=self.input_shape)
         x = input
-        x = Conv3D(self.n_filters, self.kernel_size, padding='same', kernel_initializer='he_normal')(x)
+        x = Conv3D(self.n_filters, self.kernel_size, padding='same', kernel_initializer=self.initializer)(x)
         x = Activation(self.activation)(x)
-        x = Conv3D(self.n_filters, self.kernel_size, padding='same', kernel_initializer='he_normal')(x)
+        x = Conv3D(self.n_filters, self.kernel_size, padding='same', kernel_initializer=self.initializer)(x)
         x = Activation(self.activation)(x)
         #maxpooling
         x = MaxPooling3D()(x)
         #dropout
         x = Dropout(self.dropout)(x)
         #2nd conv layer
-        x = Conv3D(self.n_filters*2, self.kernel_size, padding='same', kernel_initializer='he_normal')(x)
+        x = Conv3D(self.n_filters*2, self.kernel_size, padding='same', kernel_initializer=self.initializer)(x)
         x = Activation(self.activation)(x)
-        x = Conv3D(self.n_filters*2, self.kernel_size, padding='same', kernel_initializer='he_normal')(x)
+        x = Conv3D(self.n_filters*2, self.kernel_size, padding='same', kernel_initializer=self.initializer)(x)
         x = Activation(self.activation)(x)
         #maxpooling
         x = MaxPooling3D()(x)
+
         #dense layer
         x = Flatten()(x)
         x = Dense(128, activation='relu')(x)
-        x = Dropout(self.dropout)(x)
         x = Dense(64, activation='relu')(x)
-        x = Dropout(self.dropout)(x)
         #output layer
         output = Dense(self.n_classes)(x)
         output = BatchNormalization(momentum=0.1, epsilon=1e-05)(output)
